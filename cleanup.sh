929ffe64138179b4dc7a2f5cba95690607696dbe
therealbumface <93834820+thereal-bumface@users.noreply.github.com>
2021-11-15 15:59:14 -0800

    Add files via upload

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 echo "===================BEGIN cleanup.sh==================="
 cd /								# Navigate to root directory
-rm -rf scripts-main					# Delete scripts-main folder (and contents)
+sudo rm -rf scripts-main					# Delete scripts-main folder (and contents)
 echo "===================END cleanup.sh==================="
 
 

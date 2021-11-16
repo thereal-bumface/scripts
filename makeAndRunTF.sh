29f8822570e09ff75d3fc8d67aa38e1b980b155f
therealbumface <93834820+thereal-bumface@users.noreply.github.com>
2021-11-15 19:54:38 -0800

    Add files via upload

--- makeAndRunTF.sh
+++ makeAndRunTF.sh
@@ -2,7 +2,7 @@
 echo "===================BEGIN makeAndRunTF.sh==================="
 cd /								# Navigate to root directory
 cd /hlserver						# Navigate to "/hlserver"
-echo -e "#!/bin/sh\ntf2/srcds_run -console -game tf -timeout 0 -autoupdate -steam_dir /hlserver -steamcmd_script /hlserver/tf2_ds.txt +maxplayers 24 +map ctf_2fort +sv_pure 0" > tf.sh  	# Create "tf.sh"
+echo -e "#!/bin/sh\ntf2/srcds_run -console -game tf -timeout 0 -autoupdate -steam_dir /hlserver -steamcmd_script /hlserver/tf2_ds.txt +maxplayers 24 +map plr_hightower +sv_pure 0" > tf.sh  	# Create "tf.sh"
 sudo chmod +x tf.sh 				# Make "tf.sh" executable
 echo "===================END makeAndRunTF.sh==================="
 cd /								# Navigate to root directory
